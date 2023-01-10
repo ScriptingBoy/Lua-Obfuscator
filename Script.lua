899eb0a5e41c539bc23537c37595a83cd16b6526
ScriptingBoy <108127446+ScriptingBoy@users.noreply.github.com>
2023-01-10 19:50:14 +0700

    Pose

--- Script.lua
+++ Script.lua
@@ -1,1 +1,9 @@
 print("Lua Obfuscator by Pyan")
+type Script.lua > Ironbrew/netcoreapp2.0/input.txt
+cd Ironbrew/netcoreapp2.0/
+dotnet "IronBrew2 CLI.dll" "input.txt"
+cd ../..
+luac Ironbrew/netcoreapp2.0/out.lua
+lua Main.lua
+pause
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptingBoy/TurboHubV2.5/main/DakuHubV2'))()
